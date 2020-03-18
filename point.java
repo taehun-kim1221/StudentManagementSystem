d447c38df4c8aac68d753b0bb7a44b1132cc5081
taehun-kim1221 <62326270+taehun-kim1221@users.noreply.github.com>
2020-03-18 16:30:59 +0900

    Create point.java

---
+++ point.java
@@ -0,0 +1,14 @@
+package point;
+
+public class point {
+
+	public static void main(String[] args) {
+		System.out.println("hello,world");
+			
+		}
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
