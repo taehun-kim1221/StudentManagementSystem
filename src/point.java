110f61eb1db81c1a94a00a26d0b48351b3b0350f
ozi <ozi@DESKTOP-KEMO8N1>
2020-03-18 16:47:01 +0900

    Creat a menu

---
+++ src/point.java
@@ -0,0 +1,13 @@
+public class point {
+
+	public static void main(String[] args) {
+		System.out.println("***Student Management System Menu***");
+		System.out.println("1. Add Student");
+		System.out.println("2. Delete Student");
+		System.out.println("3. Edit Student");
+		System.out.println("4. View Student");
+		System.out.println("5. Exit");
+		System.out.println("Select one number between 1 - 5");
+		}
+	
+	}
